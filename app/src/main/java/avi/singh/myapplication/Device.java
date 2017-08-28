605e8fe6c538c840057c034c216148fa3dc168af
Avinash_Singh <avi13smartest@gmail.com>
2017-08-28 22:09:16 +0530

    changed Manage to Device from Nav Bar

---
+++ app/src/main/java/avi/singh/myapplication/Device.java
@@ -0,0 +1,13 @@
+package avi.singh.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Device extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device);
+    }
+}
--- app/src/main/java/avi/singh/myapplication/Main_Activity.java
+++ app/src/main/java/avi/singh/myapplication/Main_Activity.java
@@ -1,5 +1,6 @@
 package avi.singh.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -86,7 +87,9 @@ public class Main_Activity extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_device) {
+
+            startActivity(new Intent(Main_Activity.this,Device.class));
 
         } else if (id == R.id.nav_share) {
 
