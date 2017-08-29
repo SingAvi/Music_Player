bcc23383b8e29cef326f1dccd6f54e4a635561b7
Avinash_Singh <avi13smartest@gmail.com>
2017-08-29 13:36:35 +0530

    .

--- app/src/main/java/avi/singh/myapplication/Main_Activity.java
+++ app/src/main/java/avi/singh/myapplication/Main_Activity.java
@@ -13,9 +13,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class Main_Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +46,11 @@ public class Main_Activity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
+
+
 
     @Override
     public void onBackPressed() {
