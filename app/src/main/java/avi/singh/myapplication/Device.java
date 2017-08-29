be4d2122341aabdc963458e0fd62758dc0d27160
Avinash_Singh <avi13smartest@gmail.com>
2017-08-29 13:44:47 +0530

    .

--- app/src/main/java/avi/singh/myapplication/Device.java
+++ app/src/main/java/avi/singh/myapplication/Device.java
@@ -14,8 +14,7 @@ public class Device extends AppCompatActivity {
 
 
     ListView listView;
-    String [] items;
-    ArrayList<File> mySongs = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,47 +23,12 @@ public class Device extends AppCompatActivity {
 
         listView = findViewById(R.id.device_read_storage);
 
-        mySongs = findSongs(Environment.getExternalStorageDirectory());
-
-        items = new String[mySongs.size()];
-
-            for (int i = 0 ; i <mySongs.size(); i++){
-
-                items[i] = mySongs.get(i).getName().toString();
-            }
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.songs_directory,R.id.textView2,items);
-        listView.setAdapter(arrayAdapter);
 
 
 
 
     }
 
-    public ArrayList<File> findSongs(File root) {
-
-        ArrayList<File> arrayList = new ArrayList<File>();
-
-
-        File[] files = root.listFiles();
-
-        for (File singleFile : files) {
-
-            if (singleFile.isDirectory() && !singleFile.isHidden()) {
-
-               arrayList.addAll( findSongs(singleFile));
-
-
-            } else {
-                if (singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav") ) {
-
-                    arrayList.add(singleFile);
-                }
-            }
-
-        }
-        return arrayList;
-    }
 
 
 
